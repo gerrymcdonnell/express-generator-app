a759d31057832c6eb2ffd8c4f475f704356d6f60
Gerry Mc Donnell <irishbloke@gmail.com>
2019-04-20 11:34:50 +0100

    created needed scripts

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+const routes = require('./routes');
+
 var app = express();
 
 // view engine setup
@@ -19,8 +21,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+app.use('/', routes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+// Main router entry point, sets up all route modules
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const express = require('express');
+const router = express.Router();
+
+const indexRouter = require('./indexRouter');
+const genresRouter = require('./genresRouter');
+const artistsRouter = require('./artistsRouter');
+const albumsRouter = require('./albumsRouter');
+
+router.use('/', indexRouter);
+router.use('/genres', genresRouter);
+router.use('/artists', artistsRouter);
+router.use('/albums', albumsRouter);
 
 module.exports = router;
